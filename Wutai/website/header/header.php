d74992d93055d53b942ee4347b0a8a4af6b2221f
Gabriel Andrade <106575932+Deploy01@users.noreply.github.com>
2024-05-10 17:55:22 -0300

    v0.1 beginning website

---
+++ Wutai/website/header/header.php
@@ -0,0 +1,41 @@
+
+    <header>
+        <a href=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqSXDxZKfy8aH0-TEhOGZjGVfNgRR-03e8WW6os6l2sQ&s" alt=""></a>
+        <div class="search__box">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Categorias
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+            <input type="text" name="" id="searchInput" placeholder="Pesquisar">
+            <button id="searchButton"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+
+        <div class="language">
+            <i class="fa-solid fa-globe"></i>
+            <span>pt-BR</span>
+        </div>
+
+        <div class="user">
+            <i class="fa-solid fa-user"></i>
+            <span href="">Entrar/Registar</span>
+        </div>
+
+        <div class="cart__shopping">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span href="">Carrinho</span>
+        </div>
+    </header>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/8aca4bf827.js" crossorigin="anonymous"></script>
+
+</html>
---
+++ Wutai/website/products/products_grid.php
@@ -0,0 +1,30 @@
+
+<section class="container text-center">
+    <div class="row">
+        <div class="col">col</div>
+        <div class="col">col</div>
+        <div class="col">col</div>
+        <div class="col">col</div>
+    </div>
+    <div class="row">
+        <div class="col-8">col-8</div>
+        <div class="col-4">col-4</div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8">.col-md-8</div>
+        <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+    </div>
+
+    <div class="row">
+        <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+        <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+        <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+    </div>
+
+    <div class="row">
+        <div class="col-6">.col-6</div>
+        <div class="col-6">.col-6</div>
+    </div>
+</div>
+</section>
